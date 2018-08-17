f21f43374bbf0f5f97f5049e43203be2f315f1cf
BalazsGabonyi <gabonyibalazs@gmail.com>
2018-08-17 15:53:16 +0200

    Fix poms, remove unused parts of examples

--- ecopass/src/main/java/info/ecopass/HelloAppEngine.java
+++ ecopass/src/main/java/info/ecopass/HelloAppEngine.java
@@ -17,34 +17,22 @@
 package info.ecopass;
 
 
-import com.google.appengine.api.utils.SystemProperty;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Properties;
 
 // With @WebServlet annotation the webapp/WEB-INF/web.xml is no longer required.
-@WebServlet(name = "HelloAppEngine", value = "/hello")
+@WebServlet(name = "HelloAppEngine", value = "/*")
 public class HelloAppEngine extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
-        Properties properties = System.getProperties();
-
         response.setContentType("text/plain");
-        response.getWriter().println("Hello App Engine - Standard using "
-                + SystemProperty.version.get() + " Java "
-                + properties.get("java.specification.version"));
+        response.getWriter().println("Hello Sustainable Earth!");
     }
 
-    public static String getInfo() {
-        return "Version: " + System.getProperty("java.version")
-                + " OS: " + System.getProperty("os.name")
-                + " User: " + System.getProperty("user.name");
-    }
 
 }
