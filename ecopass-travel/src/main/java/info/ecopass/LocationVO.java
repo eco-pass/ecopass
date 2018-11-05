2b446c6c98e6d084a725568b1d2dfd9fd2671ff8
Balazs Gabonyi <gabonyibalazs@gmail.com>
2018-11-06 00:44:31 +0100

    Move DetectedActivtiy inside LocationVO

--- ecopass-travel/src/main/java/info/ecopass/LocationVO.java
+++ ecopass-travel/src/main/java/info/ecopass/LocationVO.java
@@ -19,6 +19,28 @@ public class LocationVO {
         List<DetectedActivityVO> detectedActivityVOS;
     }
 
+    private static class DetectedActivityVO {
+
+        private Activity activity;
+        private int confidence;
+
+        private enum Activity {
+            STILL,
+            ON_FOOT,
+            WALKING,
+            IN_VEHICLE,
+            IN_ROAD_VEHICLE,
+            IN_TWO_WHEELER_VEHICLE,
+            IN_FOUR_WHEELER_VEHICLE,
+            IN_RAIL_VEHICLE,
+            ON_BICYCLE,
+            RUNNING,
+            TILTING,
+            UNKNOWN,
+
+        }
+    }
+
 }
 
 
