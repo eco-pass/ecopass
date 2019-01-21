6061655b9bc8d4ab1d6328361c38da0b4a0b1c74
Balazs Gabonyi <gabonyibalazs@gmail.com>
2019-01-21 22:35:03 +0100

    Modify ToLocations Service to return list of locations

--- ecopass-travel/src/main/java/info/ecopass/locationhistory/transform/ToLocationsService.java
+++ ecopass-travel/src/main/java/info/ecopass/locationhistory/transform/ToLocationsService.java
@@ -5,11 +5,13 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Stream;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import info.ecopass.locationhistory.model.LocationVO;
 import info.ecopass.locationhistory.model.LocationsWrapper;
 import lombok.SneakyThrows;
 
@@ -18,14 +20,15 @@ public class ToLocationsService {
 	private Gson gson = new Gson();
 
 	@SneakyThrows
-	public LocationsWrapper readFullLocationHistory(String locationHistoryPath) {
+	public List<LocationVO> readFullLocationHistory(String locationHistoryPath) {
 		StringBuilder sb = new StringBuilder();
 		try (Stream<String> jsonStream = streamResource(locationHistoryPath)) {
 			jsonStream.forEach(line -> appendAsNewLine(sb, (String) line));
 		}
 		String jsonAsString = sb.toString();
-		return gson.fromJson(jsonAsString, new TypeToken<LocationsWrapper>() {
+		LocationsWrapper locationsWrapper = gson.fromJson(jsonAsString, new TypeToken<LocationsWrapper>() {
 		}.getType());
+		return locationsWrapper.getLocations();
 	}
 
 	private static void appendAsNewLine(StringBuilder sb, String line) {
