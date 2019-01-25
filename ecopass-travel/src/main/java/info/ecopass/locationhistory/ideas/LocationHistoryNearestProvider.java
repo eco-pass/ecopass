73e67728abea1540a40528021c185d6c39de7a43
Balazs Gabonyi <gabonyibalazs@gmail.com>
2019-01-25 13:31:18 +0100

    Improve NearestProvider tests

--- ecopass-travel/src/main/java/info/ecopass/locationhistory/ideas/LocationHistoryNearestProvider.java
+++ ecopass-travel/src/main/java/info/ecopass/locationhistory/ideas/LocationHistoryNearestProvider.java
@@ -14,16 +14,8 @@ public class LocationHistoryNearestProvider {
 	}
 
 	public List<LocationVO> getNearestActivities(Date date) {
-		validateDateNotFuture(date);
 		List<LocationVO> nearbyDates = new ArrayList<>();
 		return nearbyDates;
 	}
 	
-	private void validateDateNotFuture(Date date) {
-		Date now = new Date();
-		if (date.after(now)) {
-			throw new IllegalArgumentException("Provided date should be in the past");
-		}
-	}
-
 }
--- ecopass-travel/src/test/java/info/ecopass/locationhistory/ideas/TestLocationHistoryNearestProvider.java
+++ ecopass-travel/src/test/java/info/ecopass/locationhistory/ideas/TestLocationHistoryNearestProvider.java
@@ -1,5 +1,7 @@
 package info.ecopass.locationhistory.ideas;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Date;
 import java.util.List;
 
@@ -12,22 +14,17 @@ public class TestLocationHistoryNearestProvider {
 	private static final long ACTIVITY_BEFORE = 1539623266024L;
 	private static final long ACTIVITY_AFTER = 1539623245989L;
 
-	@Test(expected = IllegalArgumentException.class)
-	public void whenFutureDate_thenThrowsException() {
-		LocationHistoryNearestProvider locationHistoryFunStuff = new LocationHistoryNearestProvider();
-		Date now = new Date();
-		long yearInMilliseconds = 365 * 24 * 60 * 60 * 1000;
-		Date future = new Date(now.getTime() + yearInMilliseconds);
-		locationHistoryFunStuff.getNearestActivities(future);
-	}
-
 	@Test
 	public void whenNoAmountSpecifiedAndDateIsAfterFirstEntry_thenReturnsPreviousAndNext() {
 		LocationHistoryNearestProvider locationHistoryFunStuff = new LocationHistoryNearestProvider();
 		long millisBetween = (ACTIVITY_BEFORE + ACTIVITY_AFTER) / 2;
 		Date dateBetween = new Date(millisBetween);
 		List<LocationVO> result = locationHistoryFunStuff.getNearestActivities(dateBetween);
-		
+		assertEquals(2, result.size());
 	}
 
+	@Test
+	public void whenFutureDateSpecified_thenReturnsLastEntry() {
+		
+	}
 }
