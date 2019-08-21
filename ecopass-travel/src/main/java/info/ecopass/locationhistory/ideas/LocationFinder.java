cc3a89fcf5d2dce6d2b586b1423d34749492c539
Balazs Gabonyi <gabonyibalazs@gmail.com>
2019-08-21 17:58:36 +0200

    Add validation with tests

--- ecopass-travel/src/main/java/info/ecopass/locationhistory/ideas/LocationFinder.java
+++ ecopass-travel/src/main/java/info/ecopass/locationhistory/ideas/LocationFinder.java
@@ -2,9 +2,16 @@ package info.ecopass.locationhistory.ideas;
 
 import info.ecopass.locationhistory.model.Location;
 
-import java.util.List;
+import java.util.*;
 
 public class LocationFinder {
+
+    private static final String FROM_DATE_BAD_NOT_EARLIER = "From date should be earlier than To but it was ";
+    private static final String EQUAL = "equal";
+    private static final String LATER = "later";
+    private static final String DATE_CANNOT_BE_NULL = " date cannot be null";
+    private static final String FROM = "From";
+    private static final String TO = "To";
 
     public static LocationFinder createFrom(List<Location> locations) {
         return new LocationFinder();
@@ -12,5 +19,29 @@ public class LocationFinder {
 
     private LocationFinder() {
         // hide default constructor
+    }
+
+    public List<Location> getLocationsForInterval(Date start, Date end) {
+        validateArgs(start, end);
+        return null;
+    }
+
+    private void validateArgs(Date from, Date to) {
+        validateFromToNonNull(from, to);
+        validateFromEarlierThanTo(from, to);
+    }
+
+    private void validateFromToNonNull(Date from, Date to) {
+        Objects.requireNonNull(from, FROM + DATE_CANNOT_BE_NULL);
+        Objects.requireNonNull(to, TO + DATE_CANNOT_BE_NULL);
+    }
+
+    private void validateFromEarlierThanTo(Date from, Date to){
+        if (from.equals(to)){
+            throw new IllegalArgumentException(FROM_DATE_BAD_NOT_EARLIER + EQUAL);
+        }
+        if (from.after(to)) {
+            throw new IllegalArgumentException(FROM_DATE_BAD_NOT_EARLIER + LATER);
+        }
     }
 }
--- ecopass-travel/src/test/java/info/ecopass/locationhistory/ideas/LocationFinderTest.java
+++ ecopass-travel/src/test/java/info/ecopass/locationhistory/ideas/LocationFinderTest.java
@@ -2,9 +2,9 @@ package info.ecopass.locationhistory.ideas;
 
 import info.ecopass.locationhistory.common.util.LocationHistoryParser;
 import info.ecopass.locationhistory.model.Location;
-import org.junit.Test;
+import org.junit.*;
 
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.*;
 
 import static info.ecopass.locationhistory.common.TestConstants.PATH_TO_LOCATION_HISTORY;
@@ -14,15 +14,34 @@ public class LocationFinderTest {
     // The first and last timestamps in the example 5k location history JSON
     private static final long FIRST_TIMESTAMP = 1548229582682L;
     private static final long LAST_TIMESTAMP = 1548169767331L;
+    private static final long FOURTH_TIMESTAMP = 1548229222589L;
 
-    @Test (expected = IllegalArgumentException.class)
-    public void getLocationsForInterval_intervalOpenLaterOrEqualToIntervalClose_notAllowed() {
+    private List<Location> locations;
+
+
+    @BeforeClass
+    public void init() {
+        Path locationHistoryPath = Paths.get("", PATH_TO_LOCATION_HISTORY);
+        List<Location> locationHistoryPart = LocationHistoryParser.readFullLocationHistory(locationHistoryPath);
+        locations = Collections.unmodifiableList(locationHistoryPart);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getLocationsForInterval_intervalStartEqualToIntervalEnd_notAllowed() {
         List<Location> locations = LocationHistoryParser.readFullLocationHistory(Paths.get("", PATH_TO_LOCATION_HISTORY));
         LocationFinder locationFinder = LocationFinder.createFrom(locations);
+        locationFinder.getLocationsForInterval(new Date(0L), new Date(0L));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getLocationsForInterval_intervalStartAfterIntervalEnd_notAllowed() {
+        List<Location> locations = LocationHistoryParser.readFullLocationHistory(Paths.get("", PATH_TO_LOCATION_HISTORY));
+        LocationFinder locationFinder = LocationFinder.createFrom(locations);
+        locationFinder.getLocationsForInterval(new Date(1L), new Date(0L));
     }
 
     @Test
-    public void getLocationsForInterval_intervalOpenEarlierThanFirst_entriesStartWithFirst(){
+    public void getLocationsForInterval_intervalOpenEarlierThanFirst_entriesStartWithFirst() {
 
     }
 
