9f7b83a18829cfca0bad4958c90d8459f32293a6
Balazs Gabonyi <gabonyibalazs@gmail.com>
2019-10-06 22:05:26 +0200

    Add distance calc test with TODO

---
+++ ecopass-travel/src/test/java/info/ecopass/locationhistory/ideas/DistanceCalculatorTest.java
@@ -0,0 +1,9 @@
+package info.ecopass.locationhistory.ideas;
+
+public class DistanceCalculatorTest {
+
+    /**
+     * TODO : implementation of the most useful calculation model(s) found here
+     * https://www.movable-type.co.uk/scripts/latlong.html
+     */
+}
