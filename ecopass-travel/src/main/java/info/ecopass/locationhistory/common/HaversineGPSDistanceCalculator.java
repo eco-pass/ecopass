0f20009faf775109b1bd56a519f208cf7f167fcc
Julia Kecskemeti <kecskemeti.juliaanita@dss.hu>
2019-10-12 15:54:46 +0200

    implement calculateMeters

--- ecopass-travel/src/main/java/info/ecopass/locationhistory/common/HaversineGPSDistanceCalculator.java
+++ ecopass-travel/src/main/java/info/ecopass/locationhistory/common/HaversineGPSDistanceCalculator.java
@@ -6,7 +6,18 @@ public class HaversineGPSDistanceCalculator implements GPSDistanceCalculator {
 
     @Override
     public double calculateMeters(GPSE7Coordinate point1, GPSE7Coordinate point2) {
-        //TODO
-        return 0;
+        double R = 6371e3; // metres
+        double latitude1Radiant = Math.toRadians(point1.getLatitudeE7());
+        double latitude2Radiant = Math.toRadians(point2.getLatitudeE7());
+        double latitudeDifference = Math.toRadians(point2.getLatitudeE7() - point1.getLatitudeE7());
+        double longitudeDifference = (point2.getLongitudeE7() - point1.getLongitudeE7());
+
+        double a = Math.sin(latitudeDifference / 2) * Math.sin(latitudeDifference / 2) +
+                Math.cos(latitude1Radiant) * Math.cos(latitude2Radiant) *
+                        Math.sin(longitudeDifference / 2) * Math.sin(longitudeDifference / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        double d = R * c;
+        return d;
     }
 }
