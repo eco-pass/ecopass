a5dbd5573a0cbd41698b5d8155b83e9f372aa8a9
Balazs Gabonyi <gabonyibalazs@gmail.com>
2019-10-16 13:09:46 +0200

    Clean up test explanation comments and unused file

--- ecopass-travel/src/test/java/info/ecopass/locationhistory/common/HaversineGPSDistanceCalculatorTest.java
+++ ecopass-travel/src/test/java/info/ecopass/locationhistory/common/HaversineGPSDistanceCalculatorTest.java
@@ -7,19 +7,19 @@ import static org.junit.Assert.*;
 
 public class HaversineGPSDistanceCalculatorTest {
 
-    @Test
-    public void calculateMeters() {
-        // based on https://www.movable-type.co.uk/scripts/latlong.html
-        // haversinse the two should be 29.3 meters
-        //1564099212000, 472564732, 188930727, 3, 169, 3
-        //1564099227000, 472562328, 188932317, 3, 172, 3
-        double expectedDistance = 29.3D;
-        GPSE7Coordinate pointOne = new GPSE7Coordinate(472564732, 188930727, 169);
-        GPSE7Coordinate pointTwo = new GPSE7Coordinate(472562328, 188932317, 172);
+   @Test
+   public void calculateMetersIsCorrect() {
+      // based on https://www.movable-type.co.uk/scripts/latlong.html
+      // haversine of the two points should be 29.3 meters
+      // latitudeE7: 472564732, longitudeE7: 188930727, accuracy: 3, altitude: 169, verticalAccuracy: 3
+      // latitudeE7: 472562328, longitudeE7: 188932317, accuracy: 3, altitude: 172, verticalAccuracy: 3
+      double expectedDistance = 29.3D;
+      GPSE7Coordinate pointOne = new GPSE7Coordinate(472564732, 188930727, 169);
+      GPSE7Coordinate pointTwo = new GPSE7Coordinate(472562328, 188932317, 172);
 
 
-        HaversineGPSDistanceCalculator haversineGPSDistanceCalculator = new HaversineGPSDistanceCalculator();
-        double actualDistance = haversineGPSDistanceCalculator.calculateMeters(pointOne, pointTwo);
-        assertEquals(expectedDistance, actualDistance, 0.1D);
-    }
+      HaversineGPSDistanceCalculator haversineGPSDistanceCalculator = new HaversineGPSDistanceCalculator();
+      double actualDistance = haversineGPSDistanceCalculator.calculateMeters(pointOne, pointTwo);
+      assertEquals(expectedDistance, actualDistance, 0.1D);
+   }
 }
--- ecopass-travel/src/test/java/info/ecopass/locationhistory/ideas/DistanceCalculatorTest.java
+++
@@ -1,9 +0,0 @@
-package info.ecopass.locationhistory.ideas;
-
-public class DistanceCalculatorTest {
-
-    /**
-     * TODO : implementation of the most useful calculation model(s) found here
-     * https://www.movable-type.co.uk/scripts/latlong.html
-     */
-}
