0c100e40120e3a497352731f26bb7268fe7ae6d2
Balazs Gabonyi <gabonyibalazs@gmail.com>
2019-10-16 19:15:10 +0200

    Extract parts from Haversine. Add long distance test.

--- ecopass-travel/src/main/java/info/ecopass/locationhistory/common/HaversineGPSDistanceCalculator.java
+++ ecopass-travel/src/main/java/info/ecopass/locationhistory/common/HaversineGPSDistanceCalculator.java
@@ -9,19 +9,24 @@ public class HaversineGPSDistanceCalculator implements GPSDistanceCalculator {
       final double earthRadius = 6371e3; // metres
 
       double latitudeE7Point1 = point1.getLatitude();
-      double longitudeE7Point1 = point1.getLongitude();
-
       double latitudeE7Point2 = point2.getLatitude();
+
+      double longitudeE7Point1 = point1.getLongitude();
       double longitudeE7Point2 = point2.getLongitude();
 
-      double latitude1Radians = Math.toRadians(latitudeE7Point1);
-      double latitude2Radians = Math.toRadians(latitudeE7Point2);
       double latitudeDifference = Math.toRadians(latitudeE7Point2 - latitudeE7Point1);
       double longitudeDifference = (longitudeE7Point2 - longitudeE7Point1);
 
-      double a = Math.sin(latitudeDifference / 2) * Math.sin(latitudeDifference / 2) +
-            Math.cos(latitude1Radians) * Math.cos(latitude2Radians) *
-                  Math.sin(longitudeDifference / 2) * Math.sin(longitudeDifference / 2);
+      double latitude1Radians = Math.toRadians(latitudeE7Point1);
+      double latitude2Radians = Math.toRadians(latitudeE7Point2);
+      System.out.println(latitude1Radians);
+
+      double sinusSquareHalfLatDiff = Math.sin(latitudeDifference / 2) * Math.sin(latitudeDifference / 2);
+      double cosineProductOfLatitudes = Math.cos(latitude1Radians) * Math.cos(latitude2Radians);
+      double sineSquareHalfLongDiff = Math.sin(longitudeDifference / 2) * Math.sin(longitudeDifference / 2);
+
+      double a = sinusSquareHalfLatDiff + cosineProductOfLatitudes * sineSquareHalfLongDiff;
+
       double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
       double d = earthRadius * c;
--- ecopass-travel/src/test/java/info/ecopass/locationhistory/common/HaversineGPSDistanceCalculatorTest.java
+++ ecopass-travel/src/test/java/info/ecopass/locationhistory/common/HaversineGPSDistanceCalculatorTest.java
@@ -7,15 +7,30 @@ import static org.junit.Assert.*;
 
 public class HaversineGPSDistanceCalculatorTest {
 
+   // based on https://www.movable-type.co.uk/scripts/latlong.html
+
    @Test
-   public void calculateMetersIsCorrect() {
-      // based on https://www.movable-type.co.uk/scripts/latlong.html
+   public void calculateMetersIsCorrectShortDistance() {
+
       // haversine of the two points should be 29.3 meters
       // latitudeE7: 472564732, longitudeE7: 188930727, accuracy: 3, altitude: 169, verticalAccuracy: 3
       // latitudeE7: 472562328, longitudeE7: 188932317, accuracy: 3, altitude: 172, verticalAccuracy: 3
       double expectedDistance = 29.3D;
       GPSE7Coordinate pointOne = new GPSE7Coordinate(472564732, 188930727, 169);
       GPSE7Coordinate pointTwo = new GPSE7Coordinate(472562328, 188932317, 172);
+
+
+      HaversineGPSDistanceCalculator haversineGPSDistanceCalculator = new HaversineGPSDistanceCalculator();
+      double actualDistance = haversineGPSDistanceCalculator.calculateMeters(pointOne, pointTwo);
+      assertEquals(expectedDistance, actualDistance, 0.1D);
+   }
+
+   @Test
+   public void calculateMetersIsCorrectLongDistance() {
+
+      double expectedDistance = 20020000D;
+      GPSE7Coordinate pointOne = new GPSE7Coordinate(0, 0, 169);
+      GPSE7Coordinate pointTwo = new GPSE7Coordinate(0, 1800000000, 172);
 
 
       HaversineGPSDistanceCalculator haversineGPSDistanceCalculator = new HaversineGPSDistanceCalculator();
